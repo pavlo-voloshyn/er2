e084e448d328dd7c274d94d439f25edaa944d947
Павло <pavlo.voloshyn.pz.2020@lpnu.ua>
2022-10-23 14:12:24 +0300

    Init

---
+++ OrderItemsReserver.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace OrderReserver
+{
+    public class OrderItemsReserver
+    {
+        [FunctionName("OrderItemsReserver")]
+        public async Task Run([ServiceBusTrigger("orders", Connection = "ServiceBusConnection")]string myQueueItem, ILogger log)
+        {
+            log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+
+            string Connection = Environment.GetEnvironmentVariable("Storage");
+            string containerName = Environment.GetEnvironmentVariable("ContainerName");
+            var blobClient = new BlobContainerClient(Connection, containerName);
+            var blob = blobClient.GetBlobClient(Guid.NewGuid().ToString());
+            await blob.UploadAsync(myQueueItem);
+        }
+    }
+}
